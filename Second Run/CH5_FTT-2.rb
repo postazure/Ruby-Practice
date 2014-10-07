8fc4ec5f8bb57d77ad58949448455ce04b91c26c
Max Gordon <gordonmaxc@gmail.com>
2014-10-07 15:00:17 -0700

    added thru ch6

---
+++ Second Run/CH5_FTT-2.rb
@@ -0,0 +1,16 @@
+#Bigger, better favorite number
+puts """
+Hello!!!
+Not to be too personal, but
+what is your favorite (whole) number???
+"""
+
+fav_num = gets.chomp.to_i
+
+puts """
+So #{fav_num} is really your favorite
+number. Ok, well I guess its alright.
+
+But wouldn't #{fav_num + 1} be WAY better????
+
+"""

8741c6841407a10acc1673cc211353bf86513567
Max Gordon <gordonmaxc@gmail.com>
2014-10-08 12:40:23 -0700

    Titles Improved

---
+++ Second Run/CH6_FTT-1_Angry_Boss.rb
@@ -0,0 +1,8 @@
+#Angry Boss
+puts "And what is it that you want? Huh!?"
+
+request = gets.chomp
+
+puts "WHADDAYA MEAN \"I WANT #{request.upcase}\"!?!?!?"
+puts "YOU'RE FIRED!!"
+puts "AND DON'T LET THE DOOR HIT YOU ON THE WAY OUT!!"
